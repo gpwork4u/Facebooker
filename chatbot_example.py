426b5486eefdc54e7535d9aa0f2f5476f490ba01
GP <gpwork4u@gmail.com>
2020-05-29 12:02:57 +0800

    Add chat bot example

---
+++ chatbot_example.py
@@ -0,0 +1,18 @@
+import time
+from Facebooker import facebook
+import sys
+from getpass import getpass
+fb = facebook.API()
+email = input('email:')
+password = getpass()
+fb.login(email, password)
+while True:
+    unread_chats = fb.get_unread_chat()
+    for chat in unread_chats:
+        msg = fb.get_msg(chat)[1][-1]
+        print('get chat id:%s, message:%s'%(chat, msg))
+        if msg == 'test':
+            fb.send_msg(chat, 'test')
+        else:
+            fb.send_msg(chat, 'this is fb bot')
+    time.sleep(0.01)
