3c3b901446cdbd25a82b0b2414df35726bb60561
GP <gpwork4u@gmail.com>
2020-05-31 00:56:14 +0800

    Update to 0.1.2

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Facebooker",
-    version="0.1.1",
+    version="0.1.2",
     author="gpwork4u",
     author_email="gpwork4u@gmail.com",
     description="An un official facebook api",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gpwork4u/Facebooker",
-    packages=setuptools.find_packages(),
+    packages=['Facebooker'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
