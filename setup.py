468142eaea5128213467b802c28ef1d64ea94707
GP <gpwork4u@gmail.com>
2020-05-31 13:25:24 +0800

    Update to v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Facebooker",
-    version="0.1.2",
+    version="0.1.3",
     author="gpwork4u",
     author_email="gpwork4u@gmail.com",
     description="An un official facebook api",
